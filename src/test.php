e2189ad227bb3e53092b078f4831879d4cbd8438
Nhu Bui <buinhu2103@gmail.com>
2019-12-11 12:58:27 +0200

    Deliting passwords

--- src/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "pi", "MariaPass", "DEVdb");
-$id = $_GET['id'];
-$sql= "select Id, Name, Description from recipe where id = " . $id;
-$result = $conn->query($sql);
-while($row = $result->fetch_assoc()) {
-echo $row['Id'] . "<br/><br/>" . $row['Name'] . "<br/><br/>" . $row['Description'];
-}
-?>
