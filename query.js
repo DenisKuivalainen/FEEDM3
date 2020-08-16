796e9793f7299ae6ff662fc708a3e7803d5fcf15
AA0869 <aa0869@student.jamk.fi>
2020-08-16 17:00:10 +0300

    updated query

--- query.js
+++ query.js
@@ -6,13 +6,13 @@ function t(req) {
     let i3 = req.query.ing3;
 
     if(i3 !== '') {
-        return 'select topic as top, descript as dis from food where (ing1 = $1 or ing2 = $1 or ing3 = $1) and (ing1 = $2 or ing2 = $2 or ing3 = $2) and (ing1 = $3 or ing2 = $3 or ing3 = $3) order by random() LIMIT 1'
+        return 'select topic as top, descript as dis from food where (ing1 = $1 or ing2 = $1 or ing3 = $1) and (ing1 = $2 or ing2 = $2 or ing3 = $2) and (ing1 = $3 or ing2 = $3 or ing3 = $3) order by random()'
     } else if(i2 !== '') {
-        return 'select topic as top, descript as dis from food where (ing1 = $1 or ing2 = $1 or ing3 = $1) and (ing1 = $2 or ing2 = $2 or ing3 = $2) order by random() LIMIT 1';
+        return 'select topic as top, descript as dis from food where (ing1 = $1 or ing2 = $1 or ing3 = $1) and (ing1 = $2 or ing2 = $2 or ing3 = $2) order by random()';
     } else if(i1 !== '') {
-        return 'select topic as top, descript as dis from food where (ing1 = $1 or ing2 = $1 or ing3 = $1) order by random() LIMIT 1';
+        return 'select topic as top, descript as dis from food where (ing1 = $1 or ing2 = $1 or ing3 = $1) order by random()';
     } else {
-        return 'select topic as top, descript as dis from food order by random() LIMIT 1';
+        return 'select topic as top, descript as dis from food order by random()';
     }
 }
 
