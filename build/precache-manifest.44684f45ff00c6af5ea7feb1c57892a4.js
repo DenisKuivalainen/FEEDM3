90c719a1b63a46c8133cb900381cef37fbc6eb4b
AA0869 <aa0869@student.jamk.fi>
2020-08-17 10:25:19 +0300

    new build

---
+++ build/precache-manifest.44684f45ff00c6af5ea7feb1c57892a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6339cf20c04d3ae7790e454418dcdbb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9abbb2778c592a40c293",
+    "url": "/static/css/main.cd31cb32.chunk.css"
+  },
+  {
+    "revision": "3279f96be499b65b0511",
+    "url": "/static/js/2.0c280059.chunk.js"
+  },
+  {
+    "revision": "9abbb2778c592a40c293",
+    "url": "/static/js/main.76533d9b.chunk.js"
+  },
+  {
+    "revision": "902c862e1443fd622c23",
+    "url": "/static/js/runtime-main.11af767d.js"
+  },
+  {
+    "revision": "aff1273104d60398579b149f6864a28b",
+    "url": "/static/media/logo.aff12731.jpg"
+  }
+]);
